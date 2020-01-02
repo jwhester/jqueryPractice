a285e134af51f0f6667a34799edea4597ec98479
jwhester <jameshester88@gmail.com>
2020-01-02 00:23:48 -0600

    added some sweet sweet combat features

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-
-
-$(document).ready(function () {
-
-
-
-
-});
-
+const hero = $('#input').val();
+let maxHealth = 100;
+let health = 100;
 
 $("#start").click(function(){
+    const hero = $('#input').val();
+
     $("#nav").html(
         "<div id='heroBackground'>" +
         "<img id='hero' src='assets/hero.png' alt=''>" +
         "</div>" +
-        "<p></p>"
+        hero +
+        "<p id='health'>Health: "+ health + "/" + maxHealth +"</p>"
+
     );
     $("#content").html(
+        "<img src='assets/king.png' alt=''>" +
         "<h1>Chapter 1</h1>" +
         "<p class='text' style='display: none'>We find ourselves in a hidden dungeon on an unknown planet in an unknown</p>" +
         "<p class='text' style='display: none'> time and space. </p>" +
@@ -24,11 +23,14 @@ $("#start").click(function(){
     $('.text').show("slow");
     $('.next').click(function (){
 
-        const maxHealth = 100;
+        let maxHealth = 100;
         let health = 100;
         let monster = 20;
+        let monsterMax= 20;
         $("#content").html(
-            "<p id='health'>Health: "+ health + "/" + maxHealth +"</p>"+
+            "<img src='assets/wolf.png' alt=''>" +
+            "<p>Wolf</p>" +
+            "<p id='monsterHealth'>Health: "+ monster + "/" + monsterMax +"</p>" +
             "<br>"+
             "<button id='damage'>Attack</button>"
         );
@@ -37,6 +39,9 @@ $("#start").click(function(){
             health -= 10;
             $("#health").html(
                 "<p id='health'>Health: "+ health + "/" + maxHealth +"</p>"
+            );
+            $("#monsterHealth").html(
+                "<p id='monsterHealth'>Health: "+ monster + "/" + monsterMax +"</p>"
             );
             if(health <= 0){
                 alert("You're dead!")
